7c90da67461b0e668c34465353553c4f6013c29e
StelmakhAndrew <42778039+StelmakhAndrew@users.noreply.github.com>
2018-09-08 09:32:11 +0300

    test

--- Main.java
+++ Main.java
@@ -2,9 +2,7 @@ package HashMap;
 
 public class Main {
     public static void main(String[] args) {
-////            MyHash myHash = new MyHash(10);
         HashMap myHash = new HashMap();
-//        System.out.println(myHash.put(-1, -9223372036854775808L));
         System.out.println(myHash.put(Integer.MAX_VALUE, 100)
         );
         myHash.put(34,234);
@@ -12,57 +10,6 @@ public class Main {
         System.out.println(myHash);
 
         System.out.println(myHash.get(Integer.MAX_VALUE));
-//        System.out.println(myHash.put(-3, 100)
-//        );
-//        System.out.println(myHash.put(-4, 100)
-//        );
-//        System.out.println(myHash.put(-5, 100));
-//        System.out.println(myHash.put(-6, 100));
-//        System.out.println(myHash.put(-7, 100));
-//        myHash.put(-8, 100);
-//        myHash.put(-9, 100);
-//        myHash.put(-10, 100);
-//        myHash.put(-11, 100);
-//        myHash.put(-12, 100);
-//        myHash.put(-13, 100);
-//        myHash.put(-14, 100);
-//        myHash.put(-15, 100);
-//        myHash.put(-16, 100);
-//        myHash.put(-17, 100);
-//        myHash.put(-1564648, 100);
-//        myHash.put(-19, 100);
-//        myHash.put(-20, 100);
-//        myHash.put(-21, 100);
-//        myHash.put(-22, 100);
-//        myHash.put(-23, 100);
-//        myHash.put(-24, 100);
-//        myHash.put(2242355, 100);
-//        myHash.put(-2234526, 100);
-//        myHash.put(-265657, 100);
-//        myHash.put(-28, 100);
-//        myHash.put(-2147483648, 100);
-//      myHash.put(12,100);
-//myHash.put(13,100);
-//myHash.put(14,100);
-//myHash.put(15,100);
-//myHash.put(15,100);
-//myHash.put(15,100);
-//myHash.put(15,100);
-
-
-
-
-
-
-
-//myHash.get(13);
-//myHash.get(15);
-//myHash.get(-4);
-//myHash.get(-15);
-//myHash.get(-14);
-//myHash.get(13);
-//myHash.get(13);
-//        System.out.println(myHash.size());
 
     }
 }
